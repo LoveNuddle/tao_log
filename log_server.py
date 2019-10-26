09d2649d1fc1de00a58d59652fc42764423df26d
LoveNuddle <48555289+LoveNuddle@users.noreply.github.com>
2019-10-26 09:18:20 +0900

    Update log_server.py

--- log_server.py
+++ log_server.py
@@ -1,4 +1,4 @@
-import discord,traceback,getpass,os
+mport discord,traceback,getpass,os
 from discord.ext import commands
 
 async def send_error(self,error):
@@ -31,18 +31,19 @@ class MyBot(commands.Bot):
             self.load_extension(cog)
 
     async def on_ready(self):
-        for members_in_log in self.get_guild(634702862257094656).members:
-            nitro_log_server = discord.utils.get(self.get_guild(634702862257094656).roles, id=634703387119845406)
-            admin_log_server = discord.utils.get(self.get_guild(634702862257094656).roles, id=634703386473922580)
-            if nitro_log_server in members_in_log.roles and not admin_log_server in members_in_log.roles:
-                for members_official in self.get_guild(337524390155780107).members:
-                    nitro_check2 = discord.utils.get(self.get_guild(337524390155780107).roles, id=623842965747400705)
-                    if members_in_log == members_official and not nitro_check2 in members_official.roles:
-                        await members_in_log.remove_roles(nitro_log_server)
-
-                    else:
-                        await members_in_log.add_roles(nitro_log_server)
         try:
+            for members_in_log in self.get_guild(634702862257094656).members:
+                nitro_log_server = discord.utils.get(self.get_guild(634702862257094656).roles, id=634703387119845406)
+                admin_log_server = discord.utils.get(self.get_guild(634702862257094656).roles, id=634703386473922580)
+                if nitro_log_server in members_in_log.roles and not admin_log_server in members_in_log.roles:
+                    for members_official in self.get_guild(337524390155780107).members:
+                        nitro_check2 = discord.utils.get(self.get_guild(337524390155780107).roles, id=623842965747400705)
+                        if members_in_log == members_official and not nitro_check2 in members_official.roles:
+                            await members_in_log.remove_roles(nitro_log_server)
+
+                        else:
+                            await members_in_log.add_roles(nitro_log_server)
+        
             await self.change_presence(
                 activity=discord.Game(
                     name="TAO公式鯖と接続中 | &&help"
